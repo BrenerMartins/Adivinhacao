4dcd8b4cf7703840b40964a00691631dc087550d
Brener <brener_henrique@outlook.com>
2023-06-04 21:53:42 -0300

    versao01

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace adivinhacao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Jogo();
+    }
+
+    static void Jogo() {
+        Console.Clear();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Bem vindo ao Jogo de adivinhação!");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine("Digite um número para coomeçar :)");
+        
+
+        int numeroSecreto = int.Parse(Console.ReadLine());
+        numeroSecreto = 42;
+        
+        do
+        {
+            Console.Write("Digite um número entre 1 e 100: ");
+            int chute = int.Parse(Console.ReadLine());
+
+            if (chute == numeroSecreto)
+            {
+                Console.WriteLine("Parabéns! Você acertou o número.");
+                break;
+            }
+            else if (chute < numeroSecreto)
+            {
+                Console.WriteLine("O número é maior.");
+            }
+            else
+            {
+                Console.WriteLine("O número é menor.");
+            }
+
+        } while (true);
+
+    }
+}
